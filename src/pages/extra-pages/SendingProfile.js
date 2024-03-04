c1810aabe821a864c75840cc3e45b7cfe5511776
code-nigar <sharjeelsultan27@gmail.com>
2024-03-04 21:47:18 +0500

    modify: styling for sending-profile module

--- src/pages/extra-pages/SendingProfile.js
+++ src/pages/extra-pages/SendingProfile.js
@@ -42,7 +42,7 @@ const theme = createTheme({
             main: '#1890FF'
         },
         text: {
-            primary: '#ffffff'
+            primary: '#888'
         }
     },
     components: {
@@ -50,16 +50,10 @@ const theme = createTheme({
             styleOverrides: {
                 root: {
                     '& label': {
-                        color: '#ffffff'
+                        color: '#888'
                     },
                     '& input': {
-                        color: '#ffffff'
-                    },
-                    '& .MuiInput-underline:before': {
-                        borderBottomColor: '#ffffff'
-                    },
-                    '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
-                        borderBottomColor: '#ffffff'
+                        color: '#888'
                     }
                 }
             }
@@ -68,33 +62,37 @@ const theme = createTheme({
             styleOverrides: {
                 root: {
                     '& label': {
-                        color: '#ffffff'
+                        color: '#888'
                     },
                     '& .MuiSelect-icon': {
-                        color: '#ffffff'
+                        color: '#888'
                     },
                     '& .MuiInput-underline:before': {
-                        borderBottomColor: '#ffffff'
+                        borderBottomColor: '#888'
                     },
                     '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
-                        borderBottomColor: '#ffffff'
+                        borderBottomColor: '#888'
                     }
                 }
             }
         }
     }
 });
+
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
-        backgroundColor: theme.palette.common.black,
+        backgroundColor: '#89a',
         color: theme.palette.common.white,
-        fontSize: 16
+        fontSize: 15,
+        fontFamily: 'Open Sans'
     },
     [`&.${tableCellClasses.body}`]: {
         fontSize: 14,
-        color: theme.palette.common.black,
-        maxWidth: '210px',
-        overflow: 'auto'
+        color: '#444',
+        maxWidth: '100px',
+        overflow: 'auto',
+        padding: '10px 12px',
+        fontFamily: 'Open Sans'
     }
 }));
 
@@ -114,15 +112,15 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: '33%',
-    bgcolor: 'rgb(36, 41, 57)',
-    color: 'white !important',
-
+    bgcolor: '#fff',
+    color: '#888 !important',
+    maxHeight: '600px',
     boxShadow: 24,
     p: 4,
-    overflowX: 'hidden',
-    overflowY: 'auto',
+    overflowX: 'auto',
+    overflowY: 'auto'
     // minHeight: "400px",
-    maxHeight: '600px'
+    // maxHeight: "600px",
 };
 export default function SendingProfile() {
     const [open, setOpen] = React.useState(false);
@@ -304,15 +302,15 @@ export default function SendingProfile() {
             <MainCard title="Users And Groups" style={{ width: '100%', borderRadius: 0, boxShadow: '0 1px 20px 0 rgba(69, 90, 100, 0.08)', padding: '16px' }}>
                 <Card sx={{ maxWidth: 275 }} className="mb-3" style={{ color: 'black' }}>
                     <CardContent>
-                        <Typography variant="h4" style={{ color: 'black' }} gutterBottom>
+                        <Typography variant="h4" style={{ color: '#555', fontWeight: 400 }} gutterBottom>
                             Sending Profile
                         </Typography>
-                        <Typography variant="h5" component="div" color="text.secondary">
+                        <Typography variant="h5" component="div" color="#888">
                             Total Profiles : {data?.length}
                         </Typography>
                     </CardContent>
                 </Card>
-                <Button variant="contained" style={{ backgroundColor: '#58adc6', color: '#e1f1f5' }} className="mb-3" onClick={handleOpen}>
+                <Button variant="contained" className='btn btn-primary shadow px-sm-4 mb-3' onClick={handleOpen}>
                     New Profile
                 </Button>
                 {/* <Typography variant="h6" component="div" color="text.secondary">
@@ -325,7 +323,7 @@ export default function SendingProfile() {
                         <Box
                             component="form"
                             sx={{
-                                '& > :not(style)': { m: 1, width: '41ch' }
+                                '& > :not(style)': { m: 1, width: '100%' }
                             }}
                             noValidate
                             autoComplete="off"
@@ -453,9 +451,9 @@ export default function SendingProfile() {
                                             />
                                         </div>
                                         <div class="col-sm">
-                                            <Button variant="contained" color="error" fullWidth onClick={handleSubmit}>
+                                            <button className='btn btn-primary px-sm-4 bg-brand-color-1' onClick={handleSubmit}>
                                                 Add
-                                            </Button>
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
@@ -487,14 +485,12 @@ export default function SendingProfile() {
                                 {headers?.length < 1 && <p className="text-center">No data available in table</p>}
                                 <br />
                                 <div className="container d-flex">
-                                    <Button
-                                        variant="contained"
-                                        style={{ float: 'right', backgroundColor: '#58adc6', color: '#e1f1f5' }}
+                                    <button
+                                        className='btn btn-primary shadow px-sm-4 mx-auto'
                                         onClick={handleAdd}
-                                        fullWidth
                                     >
                                         Create Sending Profile
-                                    </Button>
+                                    </button>
                                 </div>
                             </ThemeProvider>
                         </Box>
@@ -549,7 +545,7 @@ export default function SendingProfile() {
                         <Box
                             component="form"
                             sx={{
-                                '& > :not(style)': { m: 1, width: '41ch' }
+                                '& > :not(style)': { m: 1, width: '100%' }
                             }}
                             noValidate
                             autoComplete="off"
